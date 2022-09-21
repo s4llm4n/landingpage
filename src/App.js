02d93e1a80d4924e78389a161d9b4341067b246c
s4llm4n <s4llm4n@gmail.com>
2022-09-21 22:58:50 +0700

    commit first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import IconLandingPage from "./assets/Logo.png";
+import Ilustration1 from "./assets/Illustration_1.png";
 
 function App() {
+  const menus = ["About", "Features", "Pricing", "Testimonial", "Help"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-white">
+      <header className="container max-w-5xl mx-auto flex flex-row pt-12 items-center space-x-36">
+        <img alt="icon-landingpage" src={IconLandingPage} className="w-36" />
+        <div className="flex-1">
+          <ul className="flex flex-row space-x-6">
+            {menus.map((val, index) => (
+              <li key={index}>{val}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="space-x-6 flex flex-row items-center">
+          <button className="font-bold">Sign In</button>
+          <button className="border border-red-500 rounded-full py-2 px-6">
+            Sign Up
+          </button>
+        </div>
       </header>
+      <main className="container max-w-5xl mx-auto grid grid-cols-2">
+        <div className="py-24">
+          <div>
+            <h1 className="font-bold text-4xl pb-5">
+              Want anything to be easy
+              <br />
+              With LaslesVPN.
+            </h1>
+            <div className="font-normal text-xs pb-12">
+              Provide a network for all your needs with ease and fun using
+              LaslessVPN discover interesting features from us.
+            </div>
+            <button className="py-4 px-16 bg-red-500 rounded-md text-white drop-shadow-3xl">
+              Get Started
+            </button>
+          </div>
+          <div>
+            <img src={Ilustration1} alt="ilustration-laslesvpn" />
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
