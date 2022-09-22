731aa152b4829f6a5b12db254ed5c32c4cb924a1
s4llm4n <s4llm4n@gmail.com>
2022-09-22 11:46:45 +0700

    commit features

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import IconLandingPage from "./assets/Logo.png";
 import Ilustration1 from "./assets/Illustration_1.png";
+import IconMaps from "./assets/location.png";
+import IconUser from "./assets/user.png";
+import IconServer from "./assets/Server.png";
+import Ilustration2 from "./assets/Illustration_2.png";
+import Check from "./assets/check.png";
 
 function App() {
   const menus = ["About", "Features", "Pricing", "Testimonial", "Help"];
+  const section3 = [
+    {
+      icon: IconMaps,
+      table: "users",
+      total: "30+",
+    },
+    {
+      icon: IconUser,
+      table: "users",
+      total: "30+",
+    },
+    {
+      icon: IconServer,
+      table: "users",
+      total: "30+",
+    },
+  ];
+
+  const features = ["Powerfull online protection.","Internet without borders.","Supercharged VPN.","No specific time limits."]
+
   return (
     <div className="bg-white">
       <header className="container max-w-5xl mx-auto flex flex-row pt-12 items-center space-x-36">
@@ -21,8 +46,8 @@ function App() {
           </button>
         </div>
       </header>
-      <main className="container max-w-5xl mx-auto grid grid-cols-2">
-        <div className="py-24">
+      <main className="container max-w-5xl mx-auto">
+        <div className=" grid grid-cols-2 py-24">
           <div>
             <h1 className="font-bold text-4xl pb-5">
               Want anything to be easy
@@ -39,6 +64,50 @@ function App() {
           </div>
           <div>
             <img src={Ilustration1} alt="ilustration-laslesvpn" />
+          </div>
+        </div>
+        <div className="grid grid-cols-3 shadow-2xl rounded-md py-4">
+          {section3.map((val, index) => {
+            return (
+            <div 
+              key={index} 
+              className={`flex flex-row py-8 space-x-6 justify-center ${
+                index + 1 !== section3.length && "border-r border-gray-200"
+              }`}
+            >
+              <div className="rounded-full bg-red-100 p-3">
+                <img alt={val.lable} src={val.icon} className="w-6 h-6" />
+              </div>
+              <div>
+                <div>{val.total}</div>
+                <div>{val.lable}</div>
+              </div>
+            </div>
+          );
+          })}
+        </div>
+        <div className="grid grid-cols-2 py-24">
+          <img src={Ilustration2} alt={"features-landingpage-vpn"} />
+          <div>
+            <div>We Provide Many Features You Can Use</div>
+            <div>
+              You can explore the features that we provide with fun and have 
+              their own functions each feature.
+            </div>
+            {features.map((val, index) => {
+              return (
+                <div className="flex space-x-3" key={index}>
+                  <img
+                    src={Check}
+                    alt="features-check-landingpage"
+                    className="w-6 h-6"
+                  />
+                  <div>
+                    {val}
+                  </div>
+                </div>
+            );
+            })}
           </div>
         </div>
       </main>
